bab10a8ee4f108df4de6d0b6e7dcf69da62bdd7c
martuwilson <williner.dev@gmail.com>
2025-07-29 12:30:25 -0300

    feat: agregar validaciones de clase para el input de creación de usuario y configurar pipes globales

--- chatter-backend/src/main.ts
+++ chatter-backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
   const logger = new Logger('Bootstrap');
 
